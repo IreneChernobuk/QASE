89713dc25d62b54625bb7d94ed7d25e7246858f3
IreneChernobuk <irinka_buc@mail.ru>
2023-06-08 13:28:50 +0300

    added Credentials

---
+++ src/test/java/constants/Credentials.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Credentials {
+    public static final String EMAIL = "irinka_buc@mail.ru";
+    public static final String PASSWORD = "HHRQQYzLCI";
+    public static final String TOKEN = "e00a8a913e83413e9312752888e09156fa0cb11f1770d67673438e5eb1a628de";
+}
